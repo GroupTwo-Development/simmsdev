78fbffdf436829550ecef6a4022a5d29b3a2328a
AJ Jardiah <ajardiahjr@gmail.com.com>
2024-10-16 13:32:14 -0500

    added gitignore

---
+++ wp-content/cache/acorn/framework/views/91d333888b9d7ad016087a14ff2e1a36.php
@@ -0,0 +1,4 @@
+<footer class="content-info">
+  <?php (dynamic_sidebar('sidebar-footer')); ?>
+</footer>
+<?php /**PATH /Users/grouptwodev/Herd/simmsdev-app/wp-content/themes/simmsdev/resources/views/sections/footer.blade.php ENDPATH**/ ?>
