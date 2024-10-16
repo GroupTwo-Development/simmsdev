35abbb05f16692725c14b6d848fd6b2e5b4526a7
AJ Jardiah <ajardiahjr@gmail.com.com>
2024-10-16 16:08:15 -0500

    update ssh

--- wp-content/themes/simmsdev/resources/views/layouts/app.blade.php
+++ wp-content/themes/simmsdev/resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @php(do_action('get_header'))
     @php(wp_head())
+
+    {{-- testinf --}}
   </head>
 
   <body @php(body_class())>
