26b3180aac08ad583f9cf5be6a5b1dc90136734e
AJ Jardiah <ajardiah@grouptwo.com>
2024-12-19 13:24:05 -0500

    updaed global fix

--- wp-content/themes/simmsdev/resources/views/components/contact-form-modal.blade.php
+++ wp-content/themes/simmsdev/resources/views/components/contact-form-modal.blade.php
@@ -1,7 +1,7 @@
 <div  class="relative">
     <!-- Button to trigger the modal -->
     <button type="button" data-fancybox="" data-src="#dialog-content"
-    {{ $attributes->merge(['class' => 'bg-simms-gold uppercase font-semibold tracking-[0.1em] text-center text-black inline-block px-3 py-3 whitespace-nowrap hover:bg-white transition-all ease-in-out']) }}>
+    {{ $attributes->merge(['class' => 'bg-simms-gold uppercase font-semibold tracking-[0.1em] text-center text-black inline-block px-3 py-3 whitespace-nowrap  transition-all ease-in-out']) }}>
        {{ $button_text }}
     </button>
 
